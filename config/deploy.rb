13415644c0bc5d38c8af3d20c2f111a48caee558
Henry Haller <henrybhaller@gmail.com>
2020-12-14 18:05:06 -0700

    Add more puma deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, "~/deploy/bitcoinoverload2"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "shared/puma.rb"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "shared"
